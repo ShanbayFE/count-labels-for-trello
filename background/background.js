9466e0d0c45742227ccfec32e9a3a29086698dbd
tianxin <tianxin.shi@shanbay.com>
2017-02-22 11:02:22 +0800

    fix(popup): fix init no default config bug

---
+++ background/background.js
@@ -0,0 +1,11 @@
+chrome.storage.local.get({ trelloConfig: null }, (result) => {
+    if (!result.trelloConfig) {
+        chrome.storage.local.set({
+            trelloConfig: {
+                doing: { 'actual-time': true, 'estimate-time': true },
+                done: { 'actual-time': true, 'estimate-time': true },
+                sprint: { 'actual-time': false, 'estimate-time': true },
+            },
+        });
+    }
+});
