486283f3008139a551d1529c2571796381d71593
huangjun <946947760@qq.com>
2017-08-15 17:43:05 +0800

    优化结构

--- content.js
+++ content.js
@@ -1,31 +1,31 @@
 /* global chrome */
-const showHoursCount = (sumToggle = true) => {
-    const arrayMethods = Object.getOwnPropertyNames(Array.prototype);
-    const attachArrayMethodsToNodeList = (methodName) => {
-        if (methodName !== 'length') {
-            NodeList.prototype[methodName] = Array.prototype[methodName];
-        }
-    };
-    arrayMethods.forEach(item => attachArrayMethodsToNodeList(item));
+const arrayMethods = Object.getOwnPropertyNames(Array.prototype);
+const attachArrayMethodsToNodeList = (methodName) => {
+    if (methodName !== 'length') {
+        NodeList.prototype[methodName] = Array.prototype[methodName];
+    }
+};
+arrayMethods.forEach(item => attachArrayMethodsToNodeList(item));
 
+const getHourCount = (labels, sumToggle) => labels.map((label) => {
+    const labelsArray = label.map(item => {
+        let memberNum = item.parentNode.parentNode.querySelectorAll('.list-card-members .member').length;
+        if (memberNum === 0) {
+            memberNum = 1;
+        }
+        return sumToggle ? parseFloat(item.innerHTML) * memberNum : parseFloat(item.innerHTML);
+    });
+    return labelsArray.length === 0 ? 0 :
+        labelsArray.reduce((pre, next) => pre + next);
+});
+
+const showHoursCount = (sumToggle = true) => {
     const listCards = document.querySelectorAll('.list-cards');
     const estimateLabels = listCards.map(list => list.querySelectorAll('.list-card:not(.hide) .card-label-yellow'));
     const actualLabels = listCards.map(list => list.querySelectorAll('.list-card:not(.hide) .card-label-green'));
 
-    const getHourCount = labels => labels.map((label) => {
-        const labelsArray = label.map(item => {
-            let memberNum = item.parentNode.parentNode.querySelectorAll('.list-card-members .member').length;
-            if (memberNum === 0) {
-                memberNum = 1;
-            }
-            return sumToggle ? parseFloat(item.innerHTML) * memberNum : parseFloat(item.innerHTML);
-        });
-        return labelsArray.length === 0 ? 0 :
-            labelsArray.reduce((pre, next) => pre + next);
-    });
-
-    const estimateTimeCounts = getHourCount(estimateLabels);
-    const actualTimeCounts = getHourCount(actualLabels);
+    const estimateTimeCounts = getHourCount(estimateLabels, sumToggle);
+    const actualTimeCounts = getHourCount(actualLabels, sumToggle);
 
     listCards.map((item, i) => {
         const countContainer = document.createElement('div');
